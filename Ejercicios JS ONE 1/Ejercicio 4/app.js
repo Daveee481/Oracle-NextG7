ab75ad4ff6991e4c88ac6814e598fd99aaa4f990
Daveee481 <166180966+Daveee481@users.noreply.github.com>
2024-07-22 16:57:55 -0500

    Delete Ejercicios JS ONE 1 directory

--- Ejercicios JS ONE 1/Ejercicio 4/app.js
+++
@@ -1,6 +0,0 @@
-/*Crea un mensaje que informe al usuario sobre el saldo de su cuenta, 
-utilizando un template string para incluir el valor del saldo.*/
-
-let valorDeCuenta = 10000;
-
-alert(`El saldo de su cuenta es de: ${valorDeCuenta}`);
--- Ejercicios JS ONE 1/Ejercicio 5/app.js
+++
@@ -1,6 +0,0 @@
-/*Pide al usuario que ingrese su nombre mediante un prompt. 
-Luego, muestra una alerta de bienvenida usando ese nombre.*/
-
-let nombre = prompt("Digita tu nombre");
-
-alert(`Bienvenido`+nombre)
